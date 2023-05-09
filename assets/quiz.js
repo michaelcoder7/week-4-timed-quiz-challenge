22c8e039a133577094d1e5be0e6e3c1d72ce9fac
Michael Coder7 <michaelokikiade@yahoo.co.uk>
2023-05-09 06:52:58 +0100

    more js code added

--- assets/quiz.js
+++ assets/quiz.js
@@ -32,4 +32,87 @@ function stopGame() {
 
 function onSaveScore(e) {
   let initials = document.getElementById("initials").value;
+
+  if (initials !== "") {
+    localStorage.setItem(initials, score);
+
+    document.getElementById("initials").value = "";
+  }
+}
+
+function onViewScores(e) {
+  window.localStorage.href = "scores.html";
+}
+
+function onSelectAnswer(e) {
+  let correctAnswer = questions[currentQuestion].answer;
+  let userAnswer = e.target.textContent;
+
+  if (correctAnswer === userAnswer) {
+    score++;
+
+    displayMessage("Correct!");
+  } else {
+    score--;
+    displayMessage("Wrong :-<");
+  }
+
+  displayQuestion();
+}
+
+function displayMessage(msg) {
+  message.textContent = msg;
+
+  setTimeout(function () {
+    message.textContent = " ";
+  }, 1000);
+}
+
+function displayQuestion() {
+  currentQuestion++;
+
+  console.log("current question is " + currentQuestion);
+
+  if (currentQuestion >= questions.length) {
+    stopGame();
+    return;
+  }
+
+  let question = questions(currentQuestion);
+  document.getElementById("question").textContent = question.title;
+
+  options.innerHTML = "";
+
+  for (let i = 0; i < question.choices.length; i++) {
+    let option = document.createElement("div");
+    option.textContent = question.choices[i];
+    option.onclick = onSelectAnswer;
+    option.classList.add("option");
+
+    options.appendChild(option);
+  }
+}
+
+function onStartGame() {
+  secondsLeft = 75;
+
+  currentQuestion = 0;
+
+  score = 0;
+
+  countdownTimer = setInterval(function () {
+    if (secondsLeft > 0) {
+      timer.textContent = secondsLeft;
+    } else {
+      stopGame();
+    }
+
+    secondsLeft--;
+  }, 1000);
+
+  welcome.style.display = "none";
+  result.style.display = "none";
+  quiz.style.display = "flex";
+
+  displayQuestion();
 }
